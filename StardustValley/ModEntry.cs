97d886f65ee2f8e3ec823c3248eeb255b82b23ae
Hudson Pace <roliatica@gmail.com>
2019-01-26 21:11:28 -0700

    Initialized the project and made a manifest.

---
+++ StardustValley/ModEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardustValley
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady) // Return if player hasn't loaded a save yet.
+            {
+                return;
+            }
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}."); // Print button press to the console.
+            if (e.Button == SButton.Space)
+            {
+                if (Game1.player.currentLocation is StardewValley.Locations.FarmHouse)
+                {
+                    this.Monitor.Log($"{Game1.player.Name} is in the farmhouse.");
+                }
+            }
+        }
+    }
+}
