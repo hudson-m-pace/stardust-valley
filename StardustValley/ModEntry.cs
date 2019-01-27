010c811c567fb551e2283eb56ceb0513e0f36a58
Hudson Pace <roliatica@gmail.com>
2019-01-26 22:48:11 -0700

    Added a *very* basic item.

--- StardustValley/ModEntry.cs
+++ StardustValley/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -7,7 +8,7 @@ using StardewValley;
 
 namespace StardustValley
 {
-    public class ModEntry : Mod
+    public class ModEntry : Mod, IAssetEditor
     {
         public override void Entry(IModHelper helper)
         {
@@ -25,8 +26,26 @@ namespace StardustValley
             {
                 if (Game1.player.currentLocation is StardewValley.Locations.FarmHouse)
                 {
-                    this.Monitor.Log($"{Game1.player.Name} is in the farmhouse.");
+                    this.Monitor.Log($"{Game1.player.Name} is in the farmhouse. Adding item to inventory.");
+                    Game1.player.addItemToInventory((Item)new StardewValley.Object(900, 1));
                 }
+            }
+        }
+
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Data/ObjectInformation"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public void Edit<T>(IAssetData asset)
+        {
+            if (asset.AssetNameEquals("Data/ObjectInformation"))
+            {
+                IDictionary<int, string> data = asset.AsDictionary<int, string>().Data;
+                data.Add(900, "Roli's First Item/0/-300/Basic -15/Roli's First Item/My first item.");
             }
         }
     }
